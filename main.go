3e11bee55a81222ab6460c65936b588910947880
amirrezaask <raskarpour@gmail.com>
2021-11-30 23:25:06 +0330

    add benchmark code

--- main.go
+++ main.go
@@ -4,28 +4,28 @@ import (
 	"context"
 	"ent_sample/models"
 	"fmt"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	ctx := context.Background()
-	models, err := models.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	models, err := models.Open("sqlite3", "file:ent?&_fk=1")
 	if err != nil {
 		panic(err)
 	}
-	err = models.Schema.Create(ctx)
-	if err != nil {
-		panic(err)
+	start := time.Now()
+	for i := 0; i < 10000; i++ {
+		err = models.Schema.Create(ctx)
+		if err != nil {
+			panic(err)
+		}
+		old := models.Student.Create().SetName("amirreza" + fmt.Sprint(i)).SetBirthYear(1998).SaveX(ctx)
+
+		amirreza := models.Student.GetX(ctx, old.ID)
+		_ = amirreza.Update().AddBirthYear(1).SaveX(ctx)
+		models.Student.DeleteOne(amirreza).ExecX(ctx)
 	}
-	amirreza := models.Student.Create().SetName("amirreza").SetBirthYear(1998).SaveX(ctx)
-
-	rizmo := models.Course.Create().SetName("rizmo").SetCapacity(20).SaveX(ctx)
-
-	amirreza.Update().AddCourses(rizmo).SaveX(ctx)
-	amirreza = models.Student.GetX(ctx, 1)
-
-	fmt.Printf("%+v\n", rizmo.QueryStudents().AllX(ctx))
-	fmt.Printf("%+v\n", amirreza.QueryCourses().AllX(ctx))
-
+	fmt.Println(time.Now().Sub(start).Milliseconds())
 }
